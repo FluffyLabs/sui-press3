7d797a4cf406c076042eaa3818985bd1fb5e68ec
Tomasz Drwięga <tomusdrw@gmail.com>
2025-12-13 15:50:10 +0100

    QA fix.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dev from "./Dev";
+import Home from "./Home";
 
 function App() {
   return (
