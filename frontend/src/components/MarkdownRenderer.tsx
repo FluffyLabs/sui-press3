33a3f6a1150cde5ac7d166fb4f0c14a460bd5c81
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2025-12-13 11:00:54 +0100

    qa fix

--- frontend/src/components/MarkdownRenderer.tsx
+++ frontend/src/components/MarkdownRenderer.tsx
@@ -1,4 +1,4 @@
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   content: string;
@@ -8,4 +8,4 @@ export const MarkdownRenderer = ({ content }: Props) => (
   <div className="markdown-content">
     <ReactMarkdown>{content}</ReactMarkdown>
   </div>
-)
+);
