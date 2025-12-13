2b8fd83b1a1ec197b6e4098a1cebc5a2440c0c4e
Mateusz Sikora <ms1qaz@gmail.com>
2025-12-13 16:58:27 +0100

    possibility to override package id

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,16 @@ import Dev from "./Dev";
 import Home from "./Home";
 import { Press3Provider } from "./providers/Press3Provider";
 
-const PACKAGE_ID =
+const DEFAULT_PACKAGE_ID =
   "0xc394806a04aca8aecae8f8550d1a535f8d880924444da2bca0c8066e11e88ca5";
+
+function getPackageId(): string {
+  return localStorage.getItem("press3_package_id") ?? DEFAULT_PACKAGE_ID;
+}
 
 function App() {
   return (
-    <Press3Provider packageId={PACKAGE_ID}>
+    <Press3Provider packageId={getPackageId()}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
