8987edb6b801e1df5a912108fa79ddc78212e752
Tomasz Drwięga <tomusdrw@gmail.com>
2025-12-14 02:17:37 +0100

    add img and raw rendererer

--- frontend/src/components/ContentRenderer.tsx
+++ frontend/src/components/ContentRenderer.tsx
@@ -1,15 +1,25 @@
+import {useMemo} from "react";
 import { useWalrusContent } from "../hooks/useWalrusContent";
 import { usePress3 } from "../providers/Press3Provider";
 import { HtmlRenderer } from "./HtmlRenderer";
 import { MarkdownRenderer } from "./MarkdownRenderer";
 import { MultiStageLoader } from "./MultiStageLoader";
 import { NotFoundPage } from "./NotFoundPage";
+import {JsonRenderer} from "./JsonRenderer";
+import {RawRenderer} from "./RawRenderer";
+import { ImgRenderer } from "./ImgRenderer";
 
-type Renderer = "html" | "markdown";
+type Renderer = "html" | "markdown" | "json" | "raw" | "img";
 
-function getRenderer(path: string): Renderer {
+function getRenderer(p: string): Renderer {
+  const path = p.toLowerCase();
   if (path.endsWith(".md")) return "markdown";
-  return "html";
+  if (path.endsWith(".html")) return "html";
+  if (path.endsWith(".json")) return "json";
+  if (path.endsWith(".png")) return "img";
+  if (path.endsWith(".jpg")) return "img";
+  if (path.endsWith(".gif")) return "img";
+  return "raw";
 }
 
 interface Props {
@@ -39,13 +49,30 @@ export function ContentRenderer({ path, className }: Props) {
   }
 
   const renderer = getRenderer(path);
+  const textContent = useMemo(() => {
+    try {
+      return new TextDecoder().decode(content);
+    } catch {
+      return '';
+    }
+  }, [content]);
 
   return (
     <div className={className}>
-      {renderer === "markdown" ? (
-        <MarkdownRenderer content={content} />
-      ) : (
-        <HtmlRenderer content={content} />
+      {renderer === "markdown" && (
+        <MarkdownRenderer content={textContent} />
+      )}
+      { renderer === "html" && (
+        <HtmlRenderer content={textContent} />
+      )}
+      { renderer === "json" && (
+        <JsonRenderer content={textContent} />
+      )}
+      { renderer === "img" && (
+        <ImgRenderer content={content} name={path} />
+      )}
+      { renderer === "raw" && (
+        <RawRenderer content={textContent} />
       )}
     </div>
   );
